08bab6d8a9a837ceee4c4fe98fd2d84560d69671
Aron Felippe Kerkhoven <43013959+Nirayuki@users.noreply.github.com>
2023-10-03 14:07:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const items = [{
     title: "Exercicio 1",
-    icon: "fa fa-file"
+    icon: "fa fa-file",
+    menu: ""
 },
 {
     title: "Exercicio 2",
@@ -11,6 +12,25 @@ const container = document.querySelector(".container");
 
 const win = document.querySelector(".window");
 const menuWindow = document.querySelector('.menu');
+const menubar = document.querySelector(".menubar");
+
+container.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+
+    console.log(e.path[0].className);
+    
+    if(e.path[0].className === "container"){
+        menubar.className = "menubar menuopen";
+        menubar.style.top = `${e.y}px`;
+        menubar.style.left = `${e.x}px`;
+    }
+}, false);
+
+container.addEventListener("click", (e) => {
+    if(e.path[0].className === "container"){
+        menubar.className = "menubar";
+    }
+})
 
 win.addEventListener("click", () => {
     if(menuWindow.className === "menu open"){
